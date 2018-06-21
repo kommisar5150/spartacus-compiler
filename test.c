1b612488a4f132723f6d17d094a71c7ef5391ff6
Chris LeBlanc <ch472030@dal.ca>
2018-06-21 13:22:54 -0300

    Adding stuff

--- test.c
+++ test.c
@@ -1,4 +1,16 @@
+int add ( int a , int b ) {
+    return a + b ;
+}
+int test ( int a , int b ) {
+    int c = a + b ;
+    c = 4 + 5 + 3 + 9 + 2 + a ;
+    return c ;
+}
 int main ( ) {
     int a = 5 ;
-    int b = a + 4 + 5 ;
+    int b = 10 ;
+    int c ;
+    c = add ( a , b ) ;
+    int d = test ( a , c ) ;
+    return 0 ;
 }
